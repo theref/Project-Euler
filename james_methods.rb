d53a49bfa2ed4566482476bd79665b150c6ea037
James Campbell <james.campbell@tanti.org.uk>
2013-04-16 21:04:16 +0100

    completes project euler 1 and adds sum_array method

--- james_methods.rb
+++ james_methods.rb
@@ -1,29 +1,36 @@
 module Maths_Methods
+
 	def is_prime?(n)
-	loop_bound = Math.sqrt(n).ceil
-	for i in 2..loop_bound
-		if n % i == 0
-			return false
+		loop_boundary = Math.sqrt(n).ceil
+		for i in 2..loop_boundary
+			if n % i == 0
+				return false
+			end
 		end
-	end
-	return true
-end
-
-def is_palindrome?(n)
-	check = n.to_s
-	if check == check.reverse
 		return true
 	end
-	return false
-end
 
-def is_multiple_of?(multiple, factor)
-	if multiple % factor == 0
-		return true
+	def is_palindrome?(n)
+		check = n.to_s
+		if check == check.reverse
+			return true
+		end
+		return false
 	end
-	return false
-end
 
+	def is_multiple_of?(multiple, factor)
+		if multiple % factor == 0
+			return true
+		end
+		return false
+	end
+
+	def sum_array(array)
+		sum = 0
+		array.each { |a| sum+=a }
+		return sum
+	end
+	
 end
 
 include Maths_Methods
--- project euler 1.rb
+++ project euler 1.rb
@@ -1,3 +1,14 @@
 require_relative "james_methods"
 
-puts Maths_Methods.is_multiple_of?(7,3)
+total = []
+
+for i in 1...1000
+	if Maths_Methods.is_multiple_of?(i,3) or Maths_Methods.is_multiple_of?(i,5)
+		total << i
+	end
+end
+
+print Maths_Methods.sum_array(total)
+
+# puts Maths_Methods.is_multiple_of?(7,3)
+# puts total
